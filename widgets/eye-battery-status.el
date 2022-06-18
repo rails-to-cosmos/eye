6040d87155d536fab95037a7d15e2c2c08b55f83
Dmitry Akatov <akatovda@gmail.com>
2022-06-18 16:04:24 +0300

    icons

--- widgets/eye-battery-status.el
+++ widgets/eye-battery-status.el
@@ -3,34 +3,118 @@
 (require 'eye)
 (require 'battery)
 
-(defconst battery-low-symbolic
-  "<svg width=\"16\" height=\"16\" version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\">
- <g fill=\"#dedede\" fill-rule=\"evenodd\">
-  <path d=\"m2 3c-1.108 0-2 0.892-2 2v6c0 1.108 0.892 2 2 2h9c1.108 0 2-0.892 2-2v-6c0-1.108-0.892-2-2-2zm0 1h9c0.554 0 1 0.446 1 1v6c0 0.554-0.446 1-1 1h-9c-0.554 0-1-0.446-1-1v-6c0-0.554 0.446-1 1-1z\" enable-background=\"new\" style=\"paint-order:stroke fill markers\"/>
-  <rect x=\"2\" y=\"5\" width=\"3\" height=\"6\" rx=\".48747\" ry=\".54584\" enable-background=\"new\" style=\"paint-order:stroke fill markers\"/>
-  <path d=\"m14 6v4a2 2 0 0 0 2-2 2 2 0 0 0-2-2z\" enable-background=\"new\" style=\"paint-order:stroke fill markers\"/>
+(defconst battery-000-charging "<svg width=\"24\" height=\"24\" enable-background=\"new\" version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\">
+ <title>Paper Symbolic Icon Theme</title>
+ <g transform=\"translate(2,4)\" fill=\"#ffffff\">
+  <path d=\"m2 3c-1.108 0-2 0.892-2 2v6c0 1.108 0.892 2 2 2h13c1.108 0 2-0.892 2-2v-6c0-1.108-0.892-2-2-2zm0 1h13c0.554 0 1 0.446 1 1v6c0 0.554-0.446 1-1 1h-13c-0.554 0-1-0.446-1-1v-6c0-0.554 0.446-1 1-1z\" enable-background=\"new\" fill-rule=\"evenodd\" style=\"paint-order:stroke fill markers\"/>
+  <path d=\"m18 6v4a2 2 0 0 0 2-2 2 2 0 0 0-2-2z\" enable-background=\"new\" fill-rule=\"evenodd\" style=\"paint-order:stroke fill markers\"/>
+  <path d=\"m8.834 4-3.334 5h3l-0.75 3h0.41602l3.334-5h-3l0.75-3z\" enable-background=\"new\"/>
  </g>
 </svg>")
 
-(defconst battery-caution-charging-symbolic "<svg width=\"16\" height=\"16\" version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\">
- <g fill=\"#dedede\">
-  <path d=\"m6.834 4-3.334 5h3l-0.75 3h0.41602l3.334-5h-3l0.75-3z\" enable-background=\"new\"/>
+(defconst battery-000 "<svg width=\"24\" height=\"24\" enable-background=\"new\" version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\">
+ <title>Paper Symbolic Icon Theme</title>
+ <g transform=\"translate(2,-4)\" fill=\"#d33636\" fill-rule=\"evenodd\">
+  <path d=\"m2 11c-1.108 0-2 0.892-2 2v6c0 1.108 0.892 2 2 2h13c1.108 0 2-0.892 2-2v-6c0-1.108-0.892-2-2-2zm0 1h13c0.554 0 1 0.446 1 1v6c0 0.554-0.446 1-1 1h-13c-0.554 0-1-0.446-1-1v-6c0-0.554 0.446-1 1-1z\" enable-background=\"new\" style=\"paint-order:stroke fill markers\"/>
+  <path d=\"m18 14v4a2 2 0 0 0 2-2 2 2 0 0 0-2-2z\" enable-background=\"new\" style=\"paint-order:stroke fill markers\"/>
+ </g>
+</svg>")
+
+(defconst battery-020-charging "<svg width=\"24\" height=\"24\" enable-background=\"new\" version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\">
+ <title>Paper Symbolic Icon Theme</title>
+ <g transform=\"translate(1,-4)\" fill=\"#ffffff\">
+  <g transform=\"translate(1,8)\">
+   <path d=\"m2 3c-1.108 0-2 0.892-2 2v6c0 1.108 0.892 2 2 2h13c1.108 0 2-0.892 2-2v-6c0-1.108-0.892-2-2-2zm0 1h13c0.554 0 1 0.446 1 1v6c0 0.554-0.446 1-1 1h-13c-0.554 0-1-0.446-1-1v-6c0-0.554 0.446-1 1-1z\" enable-background=\"new\" fill-rule=\"evenodd\" style=\"paint-order:stroke fill markers\"/>
+   <path d=\"m18 6v4a2 2 0 0 0 2-2 2 2 0 0 0-2-2z\" enable-background=\"new\" fill-rule=\"evenodd\" style=\"paint-order:stroke fill markers\"/>
+   <path d=\"m8.834 4-3.334 5h3l-0.75 3h0.41602l3.334-5h-3l0.75-3z\" enable-background=\"new\"/>
+  </g>
+  <path fill=\"#B6E63E\" d=\"m3.4824 13c-0.26703 0-0.48242 0.24253-0.48242 0.54492v4.9102c0 0.3024 0.21539 0.54492 0.48242 0.54492h2.0352c0.26703 0 0.48242-0.24253 0.48242-0.54492v-0.5918c-0.46977-0.27296-0.67079-0.90961-0.33203-1.418l0.33203-0.49805v-2.4023c0-0.3024-0.21539-0.54492-0.48242-0.54492h-2.0352z\" enable-background=\"new\" fill-rule=\"evenodd\" style=\"paint-order:stroke fill markers\"/>
+ </g>
+</svg>")
+
+(defconst battery-020 "<svg width=\"24\" height=\"24\" enable-background=\"new\" version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\">
+ <title>Paper Symbolic Icon Theme</title>
+ <g transform=\"translate(2,4)\" fill=\"#d33636\" fill-rule=\"evenodd\">
+  <rect x=\"2\" y=\"5\" width=\"3\" height=\"6\" rx=\".5048\" ry=\".54584\" style=\"paint-order:stroke fill markers\"/>
+  <path d=\"m2 3c-1.108 0-2 0.892-2 2v6c0 1.108 0.892 2 2 2h13c1.108 0 2-0.892 2-2v-6c0-1.108-0.892-2-2-2zm0 1h13c0.554 0 1 0.446 1 1v6c0 0.554-0.446 1-1 1h-13c-0.554 0-1-0.446-1-1v-6c0-0.554 0.446-1 1-1z\" enable-background=\"new\" style=\"paint-order:stroke fill markers\"/>
+  <path d=\"m18 6v4a2 2 0 0 0 2-2 2 2 0 0 0-2-2z\" enable-background=\"new\" style=\"paint-order:stroke fill markers\"/>
+ </g>
+</svg>")
+
+(defconst battery-040-charging "<svg width=\"24\" height=\"24\" enable-background=\"new\" version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\">
+ <title>Paper Symbolic Icon Theme</title>
+ <g transform=\"translate(2,4)\" fill=\"#ffffff\">
   <g fill-rule=\"evenodd\">
-   <path d=\"m2 3c-1.108 0-2 0.892-2 2v6c0 1.108 0.892 2 2 2h9c1.108 0 2-0.892 2-2v-6c0-1.108-0.892-2-2-2zm0 1h9c0.554 0 1 0.446 1 1v6c0 0.554-0.446 1-1 1h-9c-0.554 0-1-0.446-1-1v-6c0-0.554 0.446-1 1-1z\" enable-background=\"new\" style=\"paint-order:stroke fill markers\"/>
-   <path d=\"m2.4434 5c-0.24577 0-0.44336 0.24253-0.44336 0.54492v4.9102c0 0.3024 0.19759 0.54492 0.44336 0.54492h1.1133c0.24577 0 0.44336-0.24253 0.44336-0.54492v-0.45508h-0.5c-0.79859-1.711e-4 -1.2749-0.89013-0.83203-1.5547l1.332-1.998v-0.90234c0-0.3024-0.19759-0.54492-0.44336-0.54492h-1.1133z\" enable-background=\"new\" style=\"paint-order:stroke fill markers\"/>
-   <path d=\"m14 6v4a2 2 0 0 0 2-2 2 2 0 0 0-2-2z\" enable-background=\"new\" style=\"paint-order:stroke fill markers\"/>
+   <path fill=\"#B6E63E\" d=\"m2.5312 5c-0.29412 0-0.53125 0.24253-0.53125 0.54492v4.9102c0 0.30239 0.23713 0.54492 0.53125 0.54492h3.9375c0.29412 0 0.53125-0.24253 0.53125-0.54492v-0.45508h-1.5c-0.79859-1.712e-4 -1.2749-0.89013-0.83203-1.5547l2.1836-3.2754c-0.096507-0.10352-0.23169-0.16992-0.38281-0.16992h-3.9375z\" style=\"paint-order:stroke fill markers\"/>
+   <path d=\"m2 3c-1.108 0-2 0.892-2 2v6c0 1.108 0.892 2 2 2h13c1.108 0 2-0.892 2-2v-6c0-1.108-0.892-2-2-2zm0 1h13c0.554 0 1 0.446 1 1v6c0 0.554-0.446 1-1 1h-13c-0.554 0-1-0.446-1-1v-6c0-0.554 0.446-1 1-1z\" enable-background=\"new\" style=\"paint-order:stroke fill markers\"/>
+   <path d=\"m18 6v4a2 2 0 0 0 2-2 2 2 0 0 0-2-2z\" enable-background=\"new\" style=\"paint-order:stroke fill markers\"/>
+  </g>
+  <path d=\"m8.834 4-3.334 5h3l-0.75 3h0.41602l3.334-5h-3l0.75-3z\" enable-background=\"new\"/>
+ </g>
+</svg>")
+
+(defconst battery-060-charging "<svg width=\"24\" height=\"24\" enable-background=\"new\" version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\">
+ <title>Paper Symbolic Icon Theme</title>
+ <g transform=\"translate(2,4)\" fill=\"#ffffff\">
+  <path d=\"m8.834 4-3.334 5h3l-0.75 3h0.41602l3.334-5h-3l0.75-3z\" enable-background=\"new\"/>
+  <g fill-rule=\"evenodd\">
+   <path d=\"m2 3c-1.108 0-2 0.892-2 2v6c0 1.108 0.892 2 2 2h13c1.108 0 2-0.892 2-2v-6c0-1.108-0.892-2-2-2zm0 1h13c0.554 0 1 0.446 1 1v6c0 0.554-0.446 1-1 1h-13c-0.554 0-1-0.446-1-1v-6c0-0.554 0.446-1 1-1z\" enable-background=\"new\" style=\"paint-order:stroke fill markers\"/>
+   <path fill=\"#B6E63E\" d=\"m2.5508 5c-0.30514 0-0.55078 0.24564-0.55078 0.55078v4.8984c0 0.30514 0.24564 0.55078 0.55078 0.55078h4.418l0.25-1h-1.7188c-0.79859-1.712e-4 -1.2749-0.89013-0.83203-1.5547l2.2969-3.4453h-4.4141zm7.4805 0-0.25 1h1.7188c0.1877 4.03e-5 0.35566 0.052848 0.5 0.13672v-0.58594c0-0.30514-0.24564-0.55078-0.55078-0.55078h-1.418zm1.9688 3.0527-1.9648 2.9473h1.4141c0.30514 0 0.55078-0.24564 0.55078-0.55078v-2.3965z\" enable-background=\"new\" style=\"paint-order:stroke fill markers\"/>
+   <path d=\"m18 6v4a2 2 0 0 0 2-2 2 2 0 0 0-2-2z\" enable-background=\"new\" style=\"paint-order:stroke fill markers\"/>
   </g>
  </g>
 </svg>")
 
-(defconst battery-caution-symbolic "<svg width=\"16\" height=\"16\" version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\">
- <g fill=\"#d33636\" fill-rule=\"evenodd\">
-  <path d=\"m2 3c-1.108 0-2 0.892-2 2v6c0 1.108 0.892 2 2 2h9c1.108 0 2-0.892 2-2v-6c0-1.108-0.892-2-2-2zm0 1h9c0.554 0 1 0.446 1 1v6c0 0.554-0.446 1-1 1h-9c-0.554 0-1-0.446-1-1v-6c0-0.554 0.446-1 1-1z\" enable-background=\"new\" style=\"paint-order:stroke fill markers\"/>
-  <rect x=\"2\" y=\"5\" width=\"2\" height=\"6\" rx=\".52837\" ry=\".54584\" enable-background=\"new\" style=\"paint-order:stroke fill markers\"/>
-  <path d=\"m14 6v4a2 2 0 0 0 2-2 2 2 0 0 0-2-2z\" enable-background=\"new\" style=\"paint-order:stroke fill markers\"/>
+(defconst battery-060 "<svg width=\"24\" height=\"24\" enable-background=\"new\" version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\">
+ <title>Paper Symbolic Icon Theme</title>
+ <g transform=\"translate(2,4)\" fill=\"#ffffff\" fill-rule=\"evenodd\">
+  <rect x=\"2\" y=\"5\" width=\"10\" height=\"6\" rx=\".5236\" ry=\".54584\" style=\"paint-order:stroke fill markers\"/>
+  <path d=\"m2 3c-1.108 0-2 0.892-2 2v6c0 1.108 0.892 2 2 2h13c1.108 0 2-0.892 2-2v-6c0-1.108-0.892-2-2-2zm0 1h13c0.554 0 1 0.446 1 1v6c0 0.554-0.446 1-1 1h-13c-0.554 0-1-0.446-1-1v-6c0-0.554 0.446-1 1-1z\" enable-background=\"new\" style=\"paint-order:stroke fill markers\"/>
+  <path d=\"m18 6v4a2 2 0 0 0 2-2 2 2 0 0 0-2-2z\" enable-background=\"new\" style=\"paint-order:stroke fill markers\"/>
  </g>
 </svg>")
 
+(defconst battery-080-charging "<svg width=\"24\" height=\"24\" enable-background=\"new\" version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\">
+ <title>Paper Symbolic Icon Theme</title>
+ <g transform=\"translate(2,4)\" fill=\"#ffffff\">
+  <path  d=\"m8.834 4-3.334 5h3l-0.75 3h0.41602l3.334-5h-3l0.75-3z\" enable-background=\"new\"/>
+  <g fill-rule=\"evenodd\">
+   <path d=\"m2 3c-1.108 0-2 0.892-2 2v6c0 1.108 0.892 2 2 2h13c1.108 0 2-0.892 2-2v-6c0-1.108-0.892-2-2-2zm0 1h13c0.554 0 1 0.446 1 1v6c0 0.554-0.446 1-1 1h-13c-0.554 0-1-0.446-1-1v-6c0-0.554 0.446-1 1-1z\" enable-background=\"new\" style=\"paint-order:stroke fill markers\"/>
+   <path fill=\"#B6E63E\" d=\"m2.5508 5c-0.30514 0-0.55078 0.24564-0.55078 0.55078v4.8984c0 0.30514 0.24564 0.55078 0.55078 0.55078h4.418l0.25-1h-1.7188c-0.79859-1.712e-4 -1.2749-0.89013-0.83203-1.5547l2.2969-3.4453h-4.4141zm7.4805 0-0.25 1h1.7188c0.1877 4.03e-5 0.35566 0.052848 0.5 0.13672v-0.58594c0-0.30514-0.24564-0.55078-0.55078-0.55078h-1.418zm1.9688 3.0527-1.9648 2.9473h1.4141c0.30514 0 0.55078-0.24564 0.55078-0.55078v-2.3965z\" enable-background=\"new\" style=\"paint-order:stroke fill markers\"/>
+   <path d=\"m18 6v4a2 2 0 0 0 2-2 2 2 0 0 0-2-2z\" enable-background=\"new\" style=\"paint-order:stroke fill markers\"/>
+  </g>
+ </g>
+</svg>")
+
+(defconst battery-080 "<svg width=\"24\" height=\"24\" enable-background=\"new\" version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\">
+ <title>Paper Symbolic Icon Theme</title>
+ <g transform=\"translate(2,4)\" fill=\"#ffffff\" fill-rule=\"evenodd\">
+  <rect x=\"2\" y=\"5\" width=\"10\" height=\"6\" rx=\".5236\" ry=\".54584\" style=\"paint-order:stroke fill markers\"/>
+  <path d=\"m2 3c-1.108 0-2 0.892-2 2v6c0 1.108 0.892 2 2 2h13c1.108 0 2-0.892 2-2v-6c0-1.108-0.892-2-2-2zm0 1h13c0.554 0 1 0.446 1 1v6c0 0.554-0.446 1-1 1h-13c-0.554 0-1-0.446-1-1v-6c0-0.554 0.446-1 1-1z\" enable-background=\"new\" style=\"paint-order:stroke fill markers\"/>
+  <path d=\"m18 6v4a2 2 0 0 0 2-2 2 2 0 0 0-2-2z\" enable-background=\"new\" style=\"paint-order:stroke fill markers\"/>
+ </g>
+</svg>")
+
+(defconst battery-100-charging "<svg width=\"24\" height=\"24\" enable-background=\"new\" version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\">
+ <title>Paper Symbolic Icon Theme</title>
+ <g transform=\"translate(2,4)\" fill=\"#ffffff\">
+  <g fill-rule=\"evenodd\">
+   <path d=\"m2.5215 5c-0.28865 0-0.52148 0.24253-0.52148 0.54492v4.9102c0 0.30239 0.23284 0.54492 0.52148 0.54492h4.4473l0.25-1h-1.7188c-0.79859-1.712e-4 -1.2749-0.89013-0.83203-1.5547l2.2969-3.4453h-4.4434zm7.5098 0-0.25 1h1.7188c0.79859 1.713e-4 1.2749 0.89013 0.83203 1.5547l-2.2969 3.4453h4.4434c0.28865 0 0.52148-0.24253 0.52148-0.54492v-4.9102c0-0.30239-0.23284-0.54492-0.52148-0.54492h-4.4473z\" fill=\"#B6E63E\" style=\"paint-order:stroke fill markers\"/>
+   <path d=\"m2 3c-1.108 0-2 0.892-2 2v6c0 1.108 0.892 2 2 2h13c1.108 0 2-0.892 2-2v-6c0-1.108-0.892-2-2-2zm0 1h13c0.554 0 1 0.446 1 1v6c0 0.554-0.446 1-1 1h-13c-0.554 0-1-0.446-1-1v-6c0-0.554 0.446-1 1-1z\" enable-background=\"new\" style=\"paint-order:stroke fill markers\"/>
+   <path d=\"m18 6v4a2 2 0 0 0 2-2 2 2 0 0 0-2-2z\" enable-background=\"new\" style=\"paint-order:stroke fill markers\"/>
+  </g>
+  <path d=\"m8.834 4-3.334 5h3l-0.75 3h0.41602l3.334-5h-3l0.75-3z\" enable-background=\"new\"/>
+ </g>
+</svg>")
+
+(defconst battery-100 "<svg width=\"24\" height=\"24\" enable-background=\"new\" version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\">
+ <title>Paper Symbolic Icon Theme</title>
+ <g transform=\"translate(2,4)\" fill=\"#ffffff\" fill-rule=\"evenodd\">
+  <rect x=\"2\" y=\"5\" width=\"13\" height=\"6\" rx=\".52103\" ry=\".54584\" style=\"paint-order:stroke fill markers\"/>
+  <path d=\"m2 3c-1.108 0-2 0.892-2 2v6c0 1.108 0.892 2 2 2h13c1.108 0 2-0.892 2-2v-6c0-1.108-0.892-2-2-2zm0 1h13c0.554 0 1 0.446 1 1v6c0 0.554-0.446 1-1 1h-13c-0.554 0-1-0.446-1-1v-6c0-0.554 0.446-1 1-1z\" enable-background=\"new\" style=\"paint-order:stroke fill markers\"/>
+  <path d=\"m18 6v4a2 2 0 0 0 2-2 2 2 0 0 0-2-2z\" enable-background=\"new\" style=\"paint-order:stroke fill markers\"/>
+ </g>
+</svg>")
 
 ;; (with-current-buffer (get-buffer-create "*svg*")
 ;;   (setq test-svg (svg-create 16 16 :fill-color "#dedede" :fill-rule "evenodd"))
@@ -65,44 +149,25 @@
                :load (battery-format "%p%%" battery-status)
                :remaining (battery-format "%t" battery-status))))
   :lighter (lambda (context)
-             ;; (let* ((load (string-to-number (a-get context :load)))
-             ;;        (adapter-p (string= "ac" (downcase (a-get context :power)))))
-             ;;   (svg-image (multiline-svg
-             ;;               (if adapter-p "Adapter" "Battery")
-             ;;               (a-list :text (format "%d%%" load)
-             ;;                       :font-weight "bold"))))
-
-             ;; (create-image battery-low-symbolic 'svg t)
-
              (let* ((load (string-to-number (a-get context :load)))
                     (adapter-p (string= "ac" (downcase (a-get context :power))))
-                    (filename (cond
-                                (adapter-p (cond
-                                             ((> load 95) "battery-full-charging-symbolic")
-                                             ((> load 80) "battery-level-90-charging-symbolic")
-                                             ((> load 60) "battery-level-70-charging-symbolic")
-                                             ((> load 40) "battery-medium-charging-symbolic")
-                                             ((> load 30) "battery-level-30-charging-symbolic")
-                                             ((> load 5) "battery-level-10-charging-symbolic")
-                                             ((> load 0) "battery-empty-charging-symbolic")))
-                                (t (cond
-                                     ((> load 95) "battery-full-symbolic")
-                                     ((> load 80) "battery-level-90-symbolic")
-                                     ((> load 60) "battery-level-70-symbolic")
-                                     ((> load 40) "battery-level-50-symbolic")
-                                     ((> load 30) "battery-level-30-symbolic")
-                                     ((> load 15) "battery-level-20-symbolic")
-                                     ((> load 5) "battery-level-10-symbolic")
-                                     ((> load 0) "battery-empty-symbolic"))))))
-               (create-image (format "/home/akatovda/.emacs.d/stuff/eye/widgets/battery/%s.svg" filename)))))
-
-;; ((> load 90) (propertize "" 'display display 'face '(:foreground "#27ae60" :family "FontAwesome")))
-;; ((> load 60) (propertize "" 'display display 'face '(:foreground "#f1c40f" :family "FontAwesome")))
-;; ((> load 40) (propertize "" 'display display 'face '(:foreground "#f39c12" :family "FontAwesome")))
-;; ((> load 5) (propertize "" 'display display 'face '(:foreground "#e67e22" :family "FontAwesome")))
-
-
-;; (defvar eye-battery-status-data '())
+                    (data (eval (cond
+                                  (adapter-p
+                                   (cond
+                                     ((> load 90) 'battery-100-charging)
+                                     ((> load 70) 'battery-080-charging)
+                                     ((> load 50) 'battery-060-charging)
+                                     ((> load 30) 'battery-040-charging)
+                                     ((> load 10) 'battery-020-charging)
+                                     ((> load 0) 'battery-000-charging)))
+                                  (t (cond
+                                       ((> load 90) 'battery-100)
+                                       ((> load 70) 'battery-080)
+                                       ((> load 50) 'battery-060)
+                                       ((> load 30) 'battery-060)
+                                       ((> load 10) 'battery-020)
+                                       ((> load 0) 'battery-000)))))))
+               (create-image data 'svg t :scale 1))))
 
 ;; (defconst eye-battery-status-schema
 ;;   (list (make-ctbl:cmodel :title "Key")
