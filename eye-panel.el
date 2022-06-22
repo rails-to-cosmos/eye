809b76fc462ae717289ae757c88f080ee70a6a48
Dmitry Akatov <akatovda@gmail.com>
2022-06-22 22:27:32 +0300

    persistant widget

--- eye-panel.el
+++ eye-panel.el
@@ -23,7 +23,9 @@
                                          :font-size eye-panel-font-size
                                          :letter-spacing eye-panel-letter-spacing
                                          :font-weight font-weight
-                                         :fill "white")
+                                         :fill (if (eq 'dark (frame-parameter nil 'background-mode))
+                                                   "white"
+                                                 "black"))
                    for line in lines
                    collect (cond ((listp line) (a-merge config line))
                                  ((stringp line) (a-merge config (a-list :text line))))))
@@ -147,6 +149,59 @@
                                   (quote ,name) reason)
                          (message "Widget \"%s\" has been removed from `eye-panel-format'.
 Fix widget and try again." (quote ,name)))))
+
+       (defvar ,(a-get vars :timer)
+         (let ((time (current-time))
+               (repeat 1)
+               (timer (timer-create)))
+           (timer-set-time timer time repeat)
+           (timer-set-function timer (quote ,(a-get vars :daemon)))
+           timer)))))
+
+(cl-defmacro eye-def-persistant-widget (name store &key lighter persist)
+  (declare (indent 1) (debug t))
+  (let ((vars (a-list
+               :store (intern (format "eye-%s-store" name))
+               :lighter (intern (format "eye-%s-lighter" name))
+               :timer (intern (format "eye-%s-timer" name))
+               :mode (intern (format "eye-%s-mode" name))
+               :global-mode (intern (format "global-eye-%s-mode" name))
+               :daemon (intern (format "eye-%s-daemon" name))
+               :init (intern (format "eye-%s-init" name))
+               :quit (intern (format "eye-%s-quit" name)))))
+    `(progn
+       (cl-pushnew (quote ,name) eye-widgets)
+       (define-minor-mode ,(a-get vars :mode) "Widget minor mode.")
+
+       (defvar ,(a-get vars :lighter) (a-list) "Widget icon.")
+
+       (define-globalized-minor-mode ,(a-get vars :global-mode)
+           ,(a-get vars :mode) ,(a-get vars :mode) nil
+           (cond (,(a-get vars :global-mode) (timer-activate ,(a-get vars :timer)))
+                 (t (cancel-timer ,(a-get vars :timer)))))
+
+       (cl-defun ,(a-get vars :daemon) ()
+         (let ((store ,store))
+           (promise-chain store
+             (thena (setq ,(a-get vars :store) result)
+                    result)
+             (thena ,lighter)
+             (catcha (setq eye-panel-format (delq (quote ,name) eye-panel-format))
+                     (message "Widget \"%s\" has been disabled due to refresh error: %s" (quote ,name) reason))
+             (done (lambda (result)
+                     (setq ,(a-get vars :lighter) result))))))
+
+       (cl-defun ,(a-get vars :init) ()
+         (setq ,(a-get vars :store) (persist-load (quote ,(a-get vars :store))))
+         (let ((result ,(a-get vars :store)))
+           ,persist))
+
+       (cl-defun ,(a-get vars :quit) ()
+         (persist-save (quote ,(a-get vars :store))))
+
+       ;; (add-hook 'after-init-hook (function ,(a-get vars :init)))
+       (funcall (function ,(a-get vars :init)))
+       (add-hook 'kill-emacs-hook (function ,(a-get vars :quit)))
 
        (defvar ,(a-get vars :timer)
          (let ((time (current-time))
--- widgets/eye-bluetooth.el
+++ widgets/eye-bluetooth.el
@@ -3,21 +3,26 @@
 (require 'promise)
 (require 'eye-panel)
 
-(eye-def-widget bluetooth
+(eye-def-persistant-widget bluetooth
   (promise-chain (promise:make-process '("bluetoothctl" "--" "show"))
     (thena (s-split "\n" (s-join "\n" result)))
-    (thena (cl-loop for line in result
-              collect (cl-destructuring-bind (key &optional val &rest _) (s-split ": " line)
-                        (cond ((string= (s-trim key) "Powered") (a-list :powered (string= val "yes")))
-                              ((string= (s-trim key) "Discoverable") (a-list :discoverable (string= val "yes")))
-                              ((string= (s-trim key) "Pairable") (a-list :pairable (string= val "yes")))
-                              ((string= (s-trim key) "Discovering") (a-list :discovering (string= val "yes")))
-                              ((string= (s-trim key) "Name") (a-list :name val))
-                              ((string= (s-trim key) "Alias") (a-list :alias val))))
-              into params
-              finally (return (apply #'a-merge params))))
-    (thena (eyecon "Bluetooth" (a-list :text (cond ((a-get* result :powered) "on")
+    (thena (apply #'a-merge (--map
+                             (cl-destructuring-bind (key &optional val &rest _) (s-split ": " it)
+                               (let ((key (s-trim key))
+                                     (yes? (string= val "yes")))
+                                 (cond ((string= key "Powered") (a-list :powered yes?))
+                                       ((string= key "Discoverable") (a-list :discoverable yes?))
+                                       ((string= key "Pairable") (a-list :pairable yes?))
+                                       ((string= key "Discovering") (a-list :discovering yes?))
+                                       ((string= key "Name") (a-list :name val))
+                                       ((string= key "Alias") (a-list :alias val)))))
+                             result))))
+
+  :lighter (eyecon "Bluetooth" (a-list :text (cond ((a-get* result :powered) "on")
                                                    (t "off"))
-                                       :font-weight "bold")))))
+                                       :font-weight "bold"))
+
+  :persist (cond ((a-get* result :powered) (exwm-bluetooth-enabled (start-process "bluetoothctl" "*exwm*" "bluetoothctl" "--" "power" "on")))
+                 (t (start-process "bluetoothctl" "*exwm*" "bluetoothctl" "--" "power" "off"))))
 
 (provide 'eye-bluetooth)
